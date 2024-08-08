4849106e9491505bc25d30d1b1718ec9f51ea0fe
Rahul Kumar <iamrahul121@outlook.com>
2024-08-08 18:24:16 +0530

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let x = undefined;
-console.log(x);
+const promiseOne = new Promise((res, rej) => {
+  document.getElementById("btn").addEventListener("click", () => {
+    res("resoved");
+  });
+  document.getElementById("btn2").addEventListener("click", () => {
+    rej("Promises rejected");
+  });
+});
+
+console.log(promiseOne);
+
+promiseOne
+  .then((res) => console.log(res, promiseOne))
+  .catch((rej) => console.log(rej, promiseOne));
